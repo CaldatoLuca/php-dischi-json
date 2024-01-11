f1bca801cb3cef9bb2f4dd207df7ea1101a1d75c
CaldatoLuca <lucacaldato15@gmail.com>
2024-01-11 17:20:19 +0100

    feat: lista dischi in file json

--- server.php
+++ server.php
@@ -1,54 +1,11 @@
 <?php
 
-$disks = [
-    [
-        "titolo" => "Abbey Road",
-        "artista" => "The Beatles",
-        "anno" => 1969,
-        "genere" => "Rock",
-        "src" => "https://picsum.photos/200/300"
+//leggo il file disk.json
+$database = file_get_contents(__DIR__ . '/disks.json');
 
-    ],
-    [
-        "titolo" => "Thriller",
-        "artista" => "Michael Jackson",
-        "anno" => 1982,
-        "genere" => "Pop",
-        "src" => "https://picsum.photos/200/300"
-    ],
-    [
-        "titolo" => "The Dark Side of the Moon",
-        "artista" => "Pink Floyd",
-        "anno" => 1973,
-        "genere" => "Progressive Rock",
-        "src" => "https://picsum.photos/200/300"
+//lo trasfromo in php (oggi non serve, solo in caso di operazioni da fare in php)
+$disks = json_decode($database);
 
-    ],
-    [
-        "titolo" => "Rumours",
-        "artista" => "Fleetwood Mac",
-        "anno" => 1977,
-        "genere" => "Soft Rock",
-        "src" => "https://picsum.photos/200/300"
-
-    ],
-    [
-        "titolo" => "Back in Black",
-        "artista" => "AC/DC",
-        "anno" => 1980,
-        "genere" => "Hard Rock",
-        "src" => "https://picsum.photos/200/300"
-
-    ],
-    [
-        "titolo" => "A Night at the Opera",
-        "artista" => "Queen",
-        "anno" => 1975,
-        "genere" => "Rock",
-        "src" => "https://picsum.photos/200/300"
-    ],
-
-];
-
+// specifico che le informazioni passate sono di tipo json e trasfromo l' array da php a json
 header('Content-Type: application/json');
 echo json_encode($disks);
